ee73d484aa5675e31df992ac4fc45b9fe385d067
GSFgithub-2005 <darrengohsf@gmail.com>
2021-10-24 11:03:11 +0000

    second commit

---
+++ src/lib/Section.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let item1;
+    export let item2;
+    export let item3;
+    export let title;
+</script>
+
+<div>
+    <h3>{title}</h3>
+    <p>{item1}</p>
+    <p>{item2}</p>
+    <p>{item3}</p>
+</div>
